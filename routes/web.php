d8add00a0512d43242700895236e5923cd8d1113
arfan <arfanihidayat@gmail.com>
2021-04-28 11:06:15 +0800

    ok login form

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    });
 });
+
+Route::view('login', 'login')->name('login');
+Route::post('login', [LoginController::class, 'authenticate']);
