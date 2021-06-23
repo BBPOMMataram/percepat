b81b2647eb8d54b753ea15ca9e5a1f9940bbe6c7
arfan <arfanihidayat@gmail.com>
2021-06-23 11:02:25 +0800

    update view

--- resources/views/layout/topbar.blade.php
+++ resources/views/layout/topbar.blade.php
@@ -7,16 +7,16 @@
                     <i class="icon-menu menu-icon"></i>
                 </a>
             </li>
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <form class="search-bar">
                     <input type="text" class="form-control" placeholder="Enter keywords">
                     <a href="javascript:void();"><i class="icon-magnifier"></i></a>
                 </form>
-            </li>
+            </li> --}}
         </ul>
 
         <ul class="navbar-nav align-items-center right-nav-link">
-            <li class="nav-item dropdown-lg">
+            {{-- <li class="nav-item dropdown-lg">
                 <a class="nav-link dropdown-toggle dropdown-toggle-nocaret waves-effect" data-toggle="dropdown"
                     href="javascript:void();">
                     <i class="fa fa-envelope-open-o"></i></a>
@@ -35,7 +35,7 @@
                     <li class="dropdown-item"> <i class="flag-icon flag-icon-cn mr-2"></i> Chinese</li>
                     <li class="dropdown-item"> <i class="flag-icon flag-icon-de mr-2"></i> German</li>
                 </ul>
-            </li>
+            </li> --}}
             <li class="nav-item">
                 <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-toggle="dropdown" href="#">
                     <span class="user-profile"><img src="{{ auth()->user()->photo ? Storage::url(auth()->user()->photo) : 'https://via.placeholder.com/110x110' }}" class="img-circle"
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -51,7 +51,7 @@
       <div class="card-body">
         <div class="card-content p-2">
           <div class="text-center">
-            <img src="{{ asset('vendor/assets/images/logo-icon.png') }}" alt="logo icon">
+            <img src="{{ asset('vendor/assets/images/logo-icon.png') }}" alt="logo icon" width="100px">
           </div>
           <div class="card-title text-uppercase text-center py-3">Sign In</div>
           @error('failed')
