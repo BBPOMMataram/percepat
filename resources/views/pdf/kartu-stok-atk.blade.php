c718128d86018d84b7d5f439ac52f9646ef841d8
ASISTEN BBPOM MTR <arfanihidayat@gmail.com>
2026-08-31 10:07:27 +0700

    Perbaiki layout PDF Kartu Stok Persediaan
    - Perbaiki struktur tabel header agar lebih rapi dan bersih
    - Pastikan border 1px solid black seragam tanpa double border
    - Perbaiki jarak padding dan vertical alignment logo
    - Susun ulang info barang (NAMA, SATUAN, LOKASI) dengan layout yang benar
    - Perbaiki tabel konten dengan header yang konsisten

--- resources/views/pdf/kartu-stok-atk.blade.php
+++ resources/views/pdf/kartu-stok-atk.blade.php
@@ -4,191 +4,189 @@
     <meta charset="UTF-8">
     <title>Kartu Stok Persediaan - {{ $barang->name }}</title>
     <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
         body {
             font-family: Arial, sans-serif;
             font-size: 11px;
             margin: 0;
             padding: 15px 20px;
-            background-color: #fffde7;
+            background-color: #fff;
         }
-        #header {
-            border: 1px solid black;
-        }
-        #header table {
+        table {
             width: 100%;
             border-collapse: collapse;
         }
-        #header table td {
-            vertical-align: top;
+        table td, table th {
+            border: 1px solid black;
+            padding: 4px 8px;
         }
-        #logo-cell {
-            width: 90px;
-            text-align: center;
-            padding: 6px;
-            border-right: 1px solid black;
+
+        /* Header */
+        #header-table td {
             vertical-align: middle;
         }
-        #logo-cell img {
-            width: 55px;
+        #logo-cell {
+            width: 80px;
+            text-align: center;
         }
-        #meta-cell {
-            padding: 0;
+        #logo-cell img {
+            width: 50px;
         }
         #meta-cell table {
             width: 100%;
-            border-collapse: collapse;
         }
         #meta-cell table td {
-            padding: 3px 8px;
+            padding: 2px 8px;
             font-size: 10px;
-            border: 1px solid black;
-            border-top: none;
-            border-right: none;
-            white-space: nowrap;
-            line-height: 1.2;
-        }
-        #meta-cell table td:first-child {
-            width: 120px;
-            font-weight: bold;
-            border-left: none;
-        }
-        #meta-cell table tr:first-child td {
-            border-top: none;
+            border: none;
+            border-bottom: 1px solid black;
         }
         #meta-cell table tr:last-child td {
             border-bottom: none;
         }
-        #title {
+        #meta-cell table td:first-child {
+            width: 130px;
+            font-weight: bold;
+        }
+
+        /* Title */
+        #title-row td {
             text-align: center;
             font-weight: bold;
             font-size: 14px;
+            padding: 4px 0;
+        }
+
+        /* Info Barang */
+        #info-table td {
+            border: none;
             padding: 2px 0;
-            margin: 0;
-            line-height: 1.2;
-            border-top: 1px solid black;
+            font-size: 11px;
         }
-        #info-barang {
-            margin: 2px 0;
-        }
-        #info-barang table {
-            width: 100%;
-        }
-        #info-barang table td {
-            padding: 1px 0;
-            line-height: 1.2;
-        }
-        #info-barang table td:first-child {
+        #info-table .label {
             width: 60px;
         }
-        #lokasi {
+        #info-table .separator {
+            width: 10px;
+            text-align: center;
+        }
+        #info-table .lokasi-cell {
+            text-align: right;
+        }
+        #lokasi-box {
             border: 1px solid black;
-            padding: 4px 8px;
+            padding: 4px 10px;
             text-align: center;
             font-size: 10px;
-            width: 80px;
-            margin-left: auto;
+            display: inline-block;
+            min-width: 80px;
         }
-        #content table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-bottom: 10px;
-        }
-        #content table th, #content table td {
-            border: 1px solid black;
+
+        /* Content Table */
+        #content-table th {
+            font-weight: bold;
+            text-align: center;
             padding: 4px 6px;
+        }
+        #content-table td {
+            padding: 3px 6px;
             text-align: center;
         }
-        #content table th {
-            font-weight: bold;
-        }
-        #content table td.text-left {
+        #content-table td.text-left {
             text-align: left;
         }
     </style>
 </head>
 <body>
-    <div id="header">
-        <table>
-            <tr>
-                <td id="logo-cell" rowspan="4">
-                    <img src="storage/bpomri.png" alt="Logo BBPOM">
-                </td>
-                <td id="meta-cell">
-                    <table>
-                        <tr>
-                            <td>Nomor Formulir</td>
-                            <td>POM-14.01CFM.01SOP.01IK.14A.02</td>
-                        </tr>
-                        <tr>
-                            <td>Tanggal Pembuatan</td>
-                            <td>27 Juni 2019</td>
-                        </tr>
-                        <tr>
-                            <td>Nomor / Tanggal Revisi</td>
-                            <td>04 / 21 Oktober 2024</td>
-                        </tr>
-                        <tr>
-                            <td>Nama Formulir</td>
-                            <td>Form Kartu Stok Persediaan</td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-        </table>
-        <div id="title">KARTU STOK PERSEDIAAN</div>
-    </div>
+    <!-- Header: Logo + Meta -->
+    <table id="header-table">
+        <tr>
+            <td id="logo-cell" rowspan="4">
+                <img src="storage/bpomri.png" alt="Logo BBPOM">
+            </td>
+            <td id="meta-cell">
+                <table>
+                    <tr>
+                        <td>Nomor Formulir</td>
+                        <td>POM-14.01CFM.01SOP.01IK.14A.02</td>
+                    </tr>
+                    <tr>
+                        <td>Tanggal Pembuatan</td>
+                        <td>27 Juni 2019</td>
+                    </tr>
+                    <tr>
+                        <td>Nomor / Tanggal Revisi</td>
+                        <td>04 / 21 Oktober 2024</td>
+                    </tr>
+                    <tr>
+                        <td>Nama Formulir</td>
+                        <td>Form Kartu Stok Persediaan</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
 
-    <div id="info-barang">
-        <table>
-            <tr>
-                <td>NAMA</td>
-                <td>: {{ $barang->name }}</td>
-                <td style="width: 80px;"></td>
-                <td>
-                    <div id="lokasi">LOKASI<br>(no.)</div>
-                </td>
-            </tr>
-            <tr>
-                <td>SATUAN</td>
-                <td>: {{ $barang->satuan }} &lt; {{ $barang->code ?? '-' }} &gt;</td>
-                <td colspan="2"></td>
-            </tr>
-        </table>
-    </div>
+    <!-- Title -->
+    <table>
+        <tr id="title-row">
+            <td>KARTU STOK PERSEDIAAN</td>
+        </tr>
+    </table>
 
-    <div id="content">
-        <table>
-            <thead>
+    <!-- Info Barang -->
+    <table id="info-table">
+        <tr>
+            <td class="label">NAMA</td>
+            <td class="separator">:</td>
+            <td>{{ $barang->name }}</td>
+            <td class="lokasi-cell" rowspan="2">
+                <div id="lokasi-box">LOKASI<br>(no.)</div>
+            </td>
+        </tr>
+        <tr>
+            <td class="label">SATUAN</td>
+            <td class="separator">:</td>
+            <td>{{ $barang->satuan }} &lt; {{ $barang->code ?? '-' }} &gt;</td>
+        </tr>
+    </table>
+
+    <!-- Content Table -->
+    <table id="content-table">
+        <thead>
+            <tr>
+                <th>TANGGAL</th>
+                <th>KETERANGAN</th>
+                <th>TERIMA</th>
+                <th>KELUAR</th>
+                <th>JUMLAH</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php $total = 0; @endphp
+            @forelse($transaksi as $item)
+                @php
+                    $masuk = $item['tipe'] == 'masuk' ? $item['jumlah'] : 0;
+                    $keluar = $item['tipe'] == 'keluar' ? $item['jumlah'] : 0;
+                    $total = $total + $masuk - $keluar;
+                @endphp
                 <tr>
-                    <th>TANGGAL</th>
-                    <th>KETERANGAN</th>
-                    <th>TERIMA</th>
-                    <th>KELUAR</th>
-                    <th>JUMLAH</th>
+                    <td>{{ \Carbon\Carbon::parse($item['tanggal'])->isoFormat('D MMM Y') }}</td>
+                    <td class="text-left">{{ $item['keterangan'] }}</td>
+                    <td>{{ $masuk > 0 ? $masuk : '-' }}</td>
+                    <td>{{ $keluar > 0 ? $keluar : '-' }}</td>
+                    <td>{{ $total }}</td>
                 </tr>
-            </thead>
-            <tbody>
-                @php $total = 0; @endphp
-                @forelse($transaksi as $item)
-                    @php
-                        $masuk = $item['tipe'] == 'masuk' ? $item['jumlah'] : 0;
-                        $keluar = $item['tipe'] == 'keluar' ? $item['jumlah'] : 0;
-                        $total = $total + $masuk - $keluar;
-                    @endphp
-                    <tr>
-                        <td>{{ \Carbon\Carbon::parse($item['tanggal'])->isoFormat('D MMM Y') }}</td>
-                        <td class="text-left">{{ $item['keterangan'] }}</td>
-                        <td>{{ $masuk > 0 ? $masuk : '-' }}</td>
-                        <td>{{ $keluar > 0 ? $keluar : '-' }}</td>
-                        <td>{{ $total }}</td>
-                    </tr>
-                @empty
-                    <tr>
-                        <td colspan="5">Belum ada transaksi</td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
-    </div>
+            @empty
+                <tr>
+                    <td colspan="5">Belum ada transaksi</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
 </body>
 </html>
