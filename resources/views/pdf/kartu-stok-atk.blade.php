6dc9a8e30657a030b11dba96da6503c82b7bf445
ASISTEN BBPOM MTR <arfanihidayat@gmail.com>
2026-08-31 08:03:52 +0700

    fix: simplify header table structure - remove complex border CSS, fix image display

--- resources/views/pdf/kartu-stok-atk.blade.php
+++ resources/views/pdf/kartu-stok-atk.blade.php
@@ -11,71 +11,68 @@
             padding: 15px 20px;
             background-color: #fffde7;
         }
-        #header table {
+        .header-table {
             width: 100%;
             border-collapse: collapse;
-        }
-        #header table td {
-            vertical-align: top;
-        }
-        #logo-cell {
-            width: 80px;
-            text-align: center;
-            padding: 8px;
             border: 1px solid black;
         }
-        #logo-cell img {
-            width: 50px;
-        }
-        #meta-cell {
+        .header-table td {
+            vertical-align: top;
             padding: 0;
-            border-top: 1px solid black;
-            border-bottom: 1px solid black;
+        }
+        .logo-cell {
+            width: 70px;
+            text-align: center;
+            padding: 6px;
             border-right: 1px solid black;
         }
-        #meta-cell table {
+        .logo-cell img {
+            width: 45px;
+        }
+        .meta-cell {
+            padding: 0;
+        }
+        .meta-cell table {
             width: 100%;
             border-collapse: collapse;
-            border: none;
         }
-        #meta-cell table td {
-            padding: 4px 8px;
-            border: 1px solid black;
+        .meta-cell table td {
+            padding: 3px 8px;
             font-size: 10px;
+            border: 1px solid black;
             border-top: none;
-            border-bottom: none;
             border-right: none;
         }
-        #meta-cell table tr:first-child td {
-            border-top: none;
-        }
-        #meta-cell table tr:last-child td {
-            border-bottom: none;
-        }
-        #meta-cell table td:first-child {
-            width: 110px;
+        .meta-cell table td:first-child {
+            width: 100px;
             font-weight: bold;
             border-left: none;
         }
-        #title {
+        .meta-cell table tr:first-child td {
+            border-top: none;
+        }
+        .meta-cell table tr:last-child td {
+            border-bottom: none;
+        }
+        .title {
             text-align: center;
             font-weight: bold;
             font-size: 14px;
             padding: 4px 0;
         }
-        #info-barang {
+        .info-barang {
             margin: 4px 0;
         }
-        #info-barang table {
+        .info-barang table {
             width: 100%;
         }
-        #info-barang table td {
+        .info-barang table td {
             padding: 2px 0;
         }
-        #info-barang table td:first-child {
+        .info-barang table td:first-child {
             width: 60px;
         }
-        #lokasi {
+        .lokasi {
             border: 1px solid black;
             padding: 4px 8px;
             text-align: center;
@@ -83,66 +80,62 @@
             width: 80px;
             margin-left: auto;
         }
-        #content table {
+        .content-table {
             width: 100%;
             border-collapse: collapse;
             margin-bottom: 10px;
         }
-        #content table th, #content table td {
+        .content-table th, .content-table td {
             border: 1px solid black;
             padding: 4px 6px;
             text-align: center;
         }
-        #content table th {
+        .content-table th {
             font-weight: bold;
         }
-        #content table td.text-left {
+        .content-table td.text-left {
             text-align: left;
         }
     </style>
 </head>
 <body>
-    <div id="header">
-        <table>
-            <tr>
-                <td id="logo-cell" rowspan="4">
-                    <img src="storage/bpomri.png" alt="Logo BBPOM">
-                </td>
-                <td id="meta-cell">
-                    <table>
-                        <tr>
-                            <td>Nomor Formulir</td>
-                            <td>POM-14.01CFM.01SOP.01IK.14A.02</td>
-                        </tr>
-                        <tr>
-                            <td>Tanggal Pembuatan</td>
-                            <td>27 Juni 2019</td>
-                        </tr>
-                        <tr>
-                            <td>Nomor / Tanggal Revisi</td>
-                            <td>04 / 21 Oktober 2024</td>
-                        </tr>
-                        <tr>
-                            <td>Nama Formulir</td>
-                            <td>Form Kartu Stok Persediaan</td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-        </table>
-    </div>
+    <table class="header-table">
+        <tr>
+            <td class="logo-cell" rowspan="4">
+                <img src="storage/bpomri.png" alt="Logo BBPOM">
+            </td>
+            <td class="meta-cell">
+                <table>
+                    <tr>
+                        <td>Nomor Formulir</td>
+                        <td>POM-14.01CFM.01SOP.01IK.14A.02</td>
+                    </tr>
+                    <tr>
+                        <td>Tanggal Pembuatan</td>
+                        <td>27 Juni 2019</td>
+                    </tr>
+                    <tr>
+                        <td>Nomor / Tanggal Revisi</td>
+                        <td>04 / 21 Oktober 2024</td>
+                    </tr>
+                    <tr>
+                        <td>Nama Formulir</td>
+                        <td>Form Kartu Stok Persediaan</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
 
-    <div id="title">KARTU STOK PERSEDIAAN</div>
+    <div class="title">KARTU STOK PERSEDIAAN</div>
 
-    <div id="info-barang">
+    <div class="info-barang">
         <table>
             <tr>
                 <td>NAMA</td>
                 <td>: {{ $barang->name }}</td>
                 <td style="width: 80px;"></td>
-                <td>
-                    <div id="lokasi">LOKASI<br>(no.)</div>
-                </td>
+                <td><div class="lokasi">LOKASI<br>(no.)</div></td>
             </tr>
             <tr>
                 <td>SATUAN</td>
@@ -152,39 +145,37 @@
         </table>
     </div>
 
-    <div id="content">
-        <table>
-            <thead>
+    <table class="content-table">
+        <thead>
+            <tr>
+                <th>TANGGAL</th>
+                <th>KETERANGAN</th>
+                <th>TERIMA</th>
+                <th>KELUAR</th>
+                <th>JUMLAH</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php $total = 0; @endphp
+            @forelse($transaksi as $item)
+                @php
+                    $masuk = $item['tipe'] == 'masuk' ? $item['jumlah'] : 0;
+                    $keluar = $item['tipe'] == 'keluar' ? $item['jumlah'] : 0;
+                    $total = $total + $masuk - $keluar;
+                @endphp
                 <tr>
-                    <th>TANGGAL</th>
-                    <th>KETERANGAN</th>
-                    <th>TERIMA</th>
-                    <th>KELUAR</th>
-                    <th>JUMLAH</th>
+                    <td>{{ \Carbon\Carbon::parse($item['tanggal'])->isoFormat('D MMM Y') }}</td>
+                    <td class="text-left">{{ $item['keterangan'] }}</td>
+                    <td>{{ $masuk > 0 ? $masuk : '-' }}</td>
+                    <td>{{ $keluar > 0 ? $keluar : '-' }}</td>
+                    <td>{{ $total }}</td>
                 </tr>
-            </thead>
-            <tbody>
-                @php $total = 0; @endphp
-                @forelse($transaksi as $item)
-                    @php
-                        $masuk = $item['tipe'] == 'masuk' ? $item['jumlah'] : 0;
-                        $keluar = $item['tipe'] == 'keluar' ? $item['jumlah'] : 0;
-                        $total = $total + $masuk - $keluar;
-                    @endphp
-                    <tr>
-                        <td>{{ \Carbon\Carbon::parse($item['tanggal'])->isoFormat('D MMM Y') }}</td>
-                        <td class="text-left">{{ $item['keterangan'] }}</td>
-                        <td>{{ $masuk > 0 ? $masuk : '-' }}</td>
-                        <td>{{ $keluar > 0 ? $keluar : '-' }}</td>
-                        <td>{{ $total }}</td>
-                    </tr>
-                @empty
-                    <tr>
-                        <td colspan="5">Belum ada transaksi</td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
-    </div>
+            @empty
+                <tr>
+                    <td colspan="5">Belum ada transaksi</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
 </body>
 </html>
